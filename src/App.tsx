5b7cb41eb5c84fc4f8399a4f760447f3543de3ca
ffoster007 <penhnungcaphuli@gmail.com>
2026-06-17 07:52:49 +0700

    Added Tailwind Css V2

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-background text-foreground sm:items-start">
+      <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight">
+          To get started, edit App.tsx
+        </h1>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          Tauri + React + Tailwind v4
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <a
+          className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+          href="https://tauri.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tauri Docs
         </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a
+          className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+          href="https://react.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Docs
         </a>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-500">
-        Tailwind Works!
-      </h1>
-    </div>
     </main>
   );
 }
